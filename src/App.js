9327c68b97ba2985440c53e079695354d485ba05
maricaadrian <marica.adrian@yahoo.com>
2021-10-25 10:34:20 +0300

    Added shop page with collection preview components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import {Route, Switch} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.components';
 import './pages/homepage/homepage.styles.scss';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+    </Switch>
+
     </div>
   );
 }
